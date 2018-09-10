82f7ae366fb0c072b3018026381db409f4c26f37
Daniel-Jimenez <daniel.jimenez@ibm.com>
2018-09-10 16:33:10 -0500

    locate declaration

--- src/SyntacticAnalizer.java
+++ src/SyntacticAnalizer.java
@@ -23,7 +23,7 @@ public class SyntacticAnalizer {
     public static void main(String[] args) throws FileNotFoundException, IOException {
         demo tokensTable = new demo();
         
-        InputStream inputStream = new FileInputStream("C:/Users/dan_1/Desktop/code.txt");
+        InputStream inputStream = new FileInputStream("C:/Users/DANIELALEJANDROJIMEN/Desktop/code.txt");
         Reader inputStreamReader = new InputStreamReader(inputStream);
 
         ArrayList<Token> tokenTable = demo.analize(inputStreamReader);
@@ -52,65 +52,80 @@ public class SyntacticAnalizer {
     public static void analizar(ArrayList<Token> tokensTable){
     
         int line;
-        boolean checkLine = false;
+        boolean mainAppears = false;
         for(int i=0; i<tokensTable.size(); i++){
         
            line = tokensTable.get(i).getLineNum();
            System.out.println();
            System.out.println(line);
            
-           if(checkLine == false){
            
-               checkLine = true; 
-               System.out.println("");
-               System.out.println("TERM");
-               System.out.println(tokensTable.get(i).getValue());
-               if(tokensTable.get(i).getId() == "PALABRA RESERVADA"){
-                   
-                   ArrayList<Token> intermediario = new ArrayList();
+           
+          
+            System.out.println("");
+            System.out.println("TERM");
+            System.out.println(tokensTable.get(i).getValue());
+            if(tokensTable.get(i).getId() == "PALABRA RESERVADA"){
 
-                   System.out.println("Value is: " + tokensTable.get(i).getValue());
-                   
-                   if(tokensTable.get(i).getValue()== "principal"){
-                       
-                       System.out.println("paps");
-                   }else if (tokensTable.get(i).getValue().equals("entero") || tokensTable.get(i).getValue().equals("real") || tokensTable.get(i).getValue().equals("logico")){
-                       
-                       int size = 0;
-                      
-                       while(tokensTable.get(i).getLineNum() == line){
-                           intermediario.add(tokensTable.get(i));
-                           size++;
-                           i++;
-                       }
-                       i--;
-                       System.out.println("i is: " + i);
-                       System.out.println("Size is: " + size);
-                       
-                       if(size<=3){
-                           System.out.println("Podria ser una asignacion");
-                           checkLine = false;
-                       }else{
-                           System.out.println("Podria ser una funcion");
-                           isFuncion(intermediario);
-                           checkLine = false;
-                       }
-                       
-                   
-                   }
-                   
-                   System.out.println("GEEDGFDSFGFS");
-               }else if (tokensTable.get(i).getId() == "IDENTIFICADOR"){
-                   checkLine = false;
-               }else{
+                ArrayList<Token> intermediario = new ArrayList();
+
+                System.out.println("Value is: " + tokensTable.get(i).getValue());
+
+                if(tokensTable.get(i).getValue().equals("principal")){
+
+                    if(mainAppears == false ){
+                        mainAppears = true;
+                    }
+
+                    System.out.println("paps");
+                }else if (tokensTable.get(i).getValue().equals("entero") || tokensTable.get(i).getValue().equals("real") || tokensTable.get(i).getValue().equals("logico")){
+
+                    int size = 0;
+ 
+                    while(tokensTable.get(i).getLineNum() == line){
+                        intermediario.add(tokensTable.get(i));
+                        size++;
+                        i++;
+                    }
+                    //i--;
+                    System.out.println("i is: " + i);
+                    System.out.println("Size is: " + size);
+
+                    if(size<=3){
+                        System.out.println("Podria ser una declaracion");
+                        
+                    }else{
+                        System.out.println("Podria ser una funcion");
+                        if(isFuncion(intermediario)){
+
+                            if(mainAppears == false){
+                                System.out.println("todo chill con la funcion");
+                                System.out.println(" i is" + i);
+                                while (tokensTable.get(i).getValue().equals("entero") || tokensTable.get(i).getValue().equals("real") || tokensTable.get(i).getValue().equals("logico")){
+                                    line ++;
+                                    while(tokensTable.get(i).getLineNum() == line){
+                                        System.out.println("WHILE " + tokensTable.get(i).getValue());
+                                        System.out.println("DANIEEEEEEEL");
+                                        i++;
+                                    }
+                                    
+                                }
+                                i--;
+                            }else{
+                                System.out.println("Error, function is declared after main. Line: " + line + ", Character: " + i);
+                            }
+                        }
+                    }
+                }
+
+                System.out.println("GEEDGFDSFGFS");
+            }else if (tokensTable.get(i).getId() == "IDENTIFICADOR"){
                
-                   checkLine = false;
-               }
-           }
-           
-           checkLine = false;
+            }else{
+
+                
+            }
         }
-    
     }
 
     private static boolean isFuncion(ArrayList<Token> intermediario) {
@@ -120,18 +135,17 @@ public class SyntacticAnalizer {
         if(intermediario.get(1).getId().equals("IDENTIFICADOR") && intermediario.get(2).getValue().equals("(")){
             int j = 3;
             ArrayList params = new ArrayList();
-            while(!intermediario.get(j).getValue().equals(")")){
-                
-                //System.out.println(intermediario.get(j).getId());
-                
+            while(!intermediario.get(j).getValue().equals(")")){                
                 params.add(intermediario.get(j).getId());
               
                 j++;
 
             }
-            System.out.println(intermediario.get(j).getValue());
+            
+            
             if((isParams(params) == true) && (intermediario.get(j).getValue().equals(")")) && (intermediario.get(j+1).getValue().equals("{"))){
                 System.out.println("Header de la funcion correcto");
+                return true;
             }
             
         }     
@@ -146,6 +160,10 @@ public class SyntacticAnalizer {
         int comas = 0, terms = 0;
         
         boolean tipoDeDatoConVariable = false;
+        
+        if (params.isEmpty()){
+            return true;
+        }
         
         
         for(int i=0; i< params.size(); i++){
@@ -170,10 +188,6 @@ public class SyntacticAnalizer {
                 //System.out.println("Coma");
                 comas++;
             }
-            
-            
-            
-        
         }
         if((terms-1 != comas) || (tipoDeDatoConVariable == true)){
             return false;
@@ -182,6 +196,11 @@ public class SyntacticAnalizer {
         //System.out.println("Total comas: " + comas);
         
         return true;
+    }
+
+    private static void isAsignacion(ArrayList<Token> tokensTable, int line, int i) {
+        
+
     }
 
 }
